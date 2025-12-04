2289b72e442f39b3440358bb5bce2d40795f549b
Vijay Netekal <vijaynetekal28@gmail.com>
2025-12-04 21:39:16 +0530

    Initial commit

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "*",
+        origin: ["https://trade-stack-omega.vercel.app", "http://localhost:5173", "https://www.tradestack.io"],
         methods: ["GET", "POST"]
     }
 });
